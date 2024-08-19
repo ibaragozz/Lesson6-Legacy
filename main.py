4c313c74199a7aa0570a8759a14df46acd947518
ibaragozz <baragozz1@gmail.com>
2024-08-19 22:17:06 +0300

    Добавление инкапсуляции get и set

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
-users_list = []
+class User:
+    def __init__(self, ID, name, access_level='user'):
+        self._ID = ID
+        self._name = name
+        self._access_level = access_level
 
-class User():
-    def __init__(self, ID, name, access_level):
-        self.ID = ID
-        self.name = name
-        self.access_level = access_level
+    # Методы для получения и изменения ID
+    def get_ID(self):
+        return self._ID
+
+    def set_ID(self, ID):
+        self._ID = ID
+
+    # Методы для получения и изменения имени
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    # Метод для получения уровня доступа
+    def get_access_level(self):
+        return self._access_level
+
 
 class Admin(User):
-    def __init__(self, ID, name, access_level):
-        super().__init__(ID, name, access_level)
+    def __init__(self, ID, name):
+        super().__init__(ID, name, access_level='admin')
+        self._users_list = []
 
     def add_user(self, user):
-        if self.access_level == 'admin':
-            users_list.append(user.name)
-            print(f'{user.name} has been added')
+        if isinstance(user, User):
+            self._users_list.append(user)
+            print(f'{user.get_name()} добавлен в список')
 
-    def delete_user(self, user):
-        if self.access_level == 'admin' and user.name in users_list:
-            users_list.remove(user.name)
-            print(f'{user.name} has been deleted')
+    def remove_user(self, user):
+        if user in self._users_list:
+            self._users_list.remove(user)
+            print(f'{user.get_name()} удален из списка')
         else:
-            print('You are not an admin or user not found in the list')
+            print(f'{user.get_name()} не найден в списке')
 
     def show_all_users(self):
-        if self.access_level == 'admin':
-            print(users_list)
+        print("Users list:")
+        for user in self._users_list:
+            print(f'ID: {user.get_ID()}, Имя: {user.get_name()}, Уровень доступа: {user.get_access_level()}')
 
 
-admin = Admin(1, 'Alex', 'admin')
-user1 = User(2, 'Pamela', 'user')
-user2 = User(3, 'Natalie', 'user')
-user3 = User(4, 'Scarlett', 'user')
+# Пример использования:
+admin = Admin(1, 'Alex')
+user1 = User(2, 'Pamela')
+user2 = User(3, 'Natalie')
+user3 = User(4, 'Scarlett')
 
 admin.add_user(user1)
 admin.add_user(user2)
@@ -38,5 +58,5 @@ admin.add_user(user3)
 
 admin.show_all_users()
 
-admin.delete_user(user2)
+admin.remove_user(user2)
 admin.show_all_users()
